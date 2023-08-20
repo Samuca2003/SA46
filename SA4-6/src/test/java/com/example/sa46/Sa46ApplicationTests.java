77507935ffa0dda21794115a85fb7d0c69250e32
Samuca2003 <112135974+Samuca2003@users.noreply.github.com>
2023-08-20 08:55:54 -0300

    Add files via upload

---
+++ SA4-6/src/test/java/com/example/sa46/Sa46ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sa46;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sa46ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
